1619e84b250a1c87d0343ecb1eb37b292b0ef08e
Prashant <pkargathara7401@gmail.com>
2026-02-04 17:52:42 +0530

    under development

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -417,7 +417,8 @@ function Dashboard() {
           >
             <ActionButton
               label="Update Face"
-              onClick={() => setShowFaceConfirm(true)}
+              onClick={() => alert("Coming Soon!")}
+              // onClick={() => setShowFaceConfirm(true)}
               isMobile={isMobile}
             />
 
